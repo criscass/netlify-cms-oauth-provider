13b4027ed640677d05cc2d5aa083d29110b658cb
Cristian Cassetta <cristiancassetta@gmail.com>
2025-09-17 11:30:02 +0200

    trying to vibe code my way trough this

--- auth.js
+++ auth.js
@@ -1,14 +1,18 @@
 const randomstring = require('randomstring')
 
 module.exports = (oauth2) => {
-  // Authorization uri definition
-  const authorizationUri = oauth2.authorizeURL({
-    redirectURI: process.env.REDIRECT_URL,
-    scope: process.env.SCOPES || 'repo,user',
-    state: randomstring.generate(32)
-  })
-
   return (req, res, next) => {
+    const forwardedProto = req.headers['x-forwarded-proto'] || 'https'
+    const forwardedHost = req.headers['x-forwarded-host'] || req.headers.host
+    const dynamicRedirect = `${forwardedProto}://${forwardedHost}/callback`
+    const redirectURI = process.env.REDIRECT_URL || dynamicRedirect
+
+    const authorizationUri = oauth2.authorizeURL({
+      redirectURI,
+      scope: process.env.SCOPES || 'repo,user',
+      state: randomstring.generate(32)
+    })
+
     res.redirect(authorizationUri)
   }
 }
